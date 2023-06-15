3c2168a2c1819c38600915fa76a9e4b7558de000
Mark Veltzer <mark.veltzer@gmail.com>
2023-06-15 15:08:38 +0300

    started work

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+LOCAL_FOLDER="linux-4.4.157"
+LOCAL_FILE="linux-4.4.157.tar.xz"
+IMAGE="https://cdn.kernel.org/pub/linux/kernel/v4.x/${LOCAL_FILE}"
+
+if [ ! -d build ]
+then
+	mkdir build
+fi
+cd build
+if [ ! -f "${LOCAL_FILE}" ]
+then
+	wget "${IMAGE}"
+fi
+
+if [ ! -d "${LOCAL_FOLDER}" ]
+then
+	tar xvf "${LOCAL_FILE}"
+fi
