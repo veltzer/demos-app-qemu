e1c37c8f74390ab238372b90192ad2d055cfaf00
Mark Veltzer <mark.veltzer@gmail.com>
2023-06-16 20:11:20 +0300

    busybox download and extract

--- build.sh
+++ build.sh
@@ -2,26 +2,31 @@
 
 KERNEL_VERSION="6.1.34"
 KERNEL_FOLDER="v6.x"
-TYPE="allnoconfig"
-ARCH="arm"
-LOCAL_FOLDER="linux-${KERNEL_VERSION}"
-LOCAL_FILE="${LOCAL_FOLDER}.tar.xz"
-IMAGE="https://cdn.kernel.org/pub/linux/kernel/${KERNEL_FOLDER}/${LOCAL_FILE}"
-STOP_AFTER_TAR=0
+KERNEL_TYPE="allnoconfig"
+KERNEL_ARCH="arm"
+KERNEL_FOLDER="linux-${KERNEL_VERSION}"
+KERNEL_FILE="${KERNEL_FOLDER}.tar.xz"
+KERNEL_DOWNLOAD="https://cdn.kernel.org/pub/linux/kernel/${KERNEL_FOLDER}/${KERNEL_FILE}"
+STOP_AFTER_TAR="0"
+
+BUSYBOX_VERSION="1.36.1"
+BUSYBOX_FOLDER="busybox-${BUSYBOX_VERSION}"
+BUSYBOX_FILE="${BUSYBOX_FOLDER}.tar.bz2"
+BUSYBOX_DOWNLOAD="https://busybox.net/downloads/${BUSYBOX_FILE}"
 
 if [ ! -d build ]
 then
 	mkdir build
 fi
 cd build
-if [ ! -f "${LOCAL_FILE}" ]
+if [ ! -f "${KERNEL_FILE}" ]
 then
-	wget "${IMAGE}"
+	wget "${KERNEL_DOWNLOAD}"
 fi
 
-if [ ! -d "${LOCAL_FOLDER}" ]
+if [ ! -d "${KERNEL_FOLDER}" ]
 then
-	tar xvf "${LOCAL_FILE}"
+	tar xvf "${KERNEL_FILE}"
 fi
 
 if [ "${STOP_AFTER_TAR}" = "1" ]
@@ -30,10 +35,21 @@ then
 	exit 1
 fi
 
-cd "${LOCAL_FOLDER}"
+cd "${KERNEL_FOLDER}"
 if [ ! -f "stamp" ]
 then
-	cp "../../kernel_config.${ARCH}.${KERNEL_VERSION}.${TYPE}" ".config"
-	make ARCH="${ARCH}" CROSS_COMPILE=arm-linux-gnueabi-
+	cp "../../kernel_config.${KERNEL_ARCH}.${KERNEL_VERSION}.${KERNEL_TYPE}" ".config"
+	make ARCH="${KERNEL_ARCH}" CROSS_COMPILE=arm-linux-gnueabi-
 	touch stamp
+fi
+cd ..
+
+if [ ! -f "${BUSYBOX_FILE}" ]
+then
+	wget "${BUSYBOX_DOWNLOAD}"
+fi
+
+if [ ! -d "${BUSYBOX_FOLDER}" ]
+then
+	tar xvf "${BUSYBOX_FILE}"
 fi
