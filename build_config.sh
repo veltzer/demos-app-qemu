998ec0cca4695f8f8bf7811a66586bfa4b7593ed
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-02 06:28:47 +0200

    no commit message given

---
+++ build_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+cd "build"
+if [ ! -d "linux-6.1.34" ]
+then
+	tar xvf "linux-6.1.34.tar.xz"
+fi
+cd "linux-6.1.34"
+make mrproper
+make defconfig
+scripts/config --disable KEYS
+# scripts/config --set-str SYSTEM_TRUSTED_KEYS ""
+# scripts/config --set-str SYSTEM_REVOCATIOND_KEYS ""
+cp .config "../../kernel_config.x86_64.6.1.34.defconfig"
