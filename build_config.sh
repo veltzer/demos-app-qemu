98235c1edcead3aea4fce0fb2ce740e53b24a7fd
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-02 07:40:30 +0200

    no commit message given

--- build_config.sh
+++ build_config.sh
@@ -9,7 +9,8 @@ make mrproper
 make defconfig
 scripts/config --disable KEYS
 scripts/config --enable BLK_DEV_RAM
-scripts/config --set-str BLK_DEV_RAM_COUNT "16"
+scripts/config --set-val BLK_DEV_RAM_COUNT 16
+scripts/config --set-val BLK_DEV_RAM_SIZE 4096
 # scripts/config --set-str SYSTEM_TRUSTED_KEYS ""
 # scripts/config --set-str SYSTEM_REVOCATIOND_KEYS ""
 cp .config "../../kernel_config.x86_64.6.1.34.defconfig"
