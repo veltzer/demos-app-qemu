0fd7f4232f537ac1dbb5e7a0a08222c0dc98697f
Mark Veltzer <mark.veltzer@gmail.com>
2024-02-14 09:08:34 +0200

    no commit message given

--- host_prepare.sh
+++ host_prepare.sh
@@ -3,7 +3,10 @@
 # copy stuff from /boot to be able to run the host images
 
 ARCH=$(arch)
-KERNEL_VERSION=$(uname -r)
+# KERNEL_VERSION=$(uname -r)
+KERNEL_VERSION="6.2.0-1005-lowlatency"
 
-sudo cp "/boot/vmlinuz" "/boot/initrd.img" build
+mkdir -p build
+sudo cp "/boot/vmlinuz-${KERNEL_VERSION}" "build/vmlinuz"
+sudo cp "/boot/initrd.img-${KERNEL_VERSION}" "build/initrd.img"
 sudo chown "$USER:$USER" "build/vmlinuz" "build/initrd.img"
