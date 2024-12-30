d58aa500bba04f91830659b694b37c945d62ffec
Mark Veltzer <mark.veltzer@gmail.com>
2024-12-30 21:17:33 +0200

    no commit message given

--- exercises/05_system_from_scratch/compile_dts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-OVERLAY="versatile-elbit.dts"
-BASE="versatile_full.dts"
-MY_DTS="merged.dts"
-# MY_DTB="versatile-elbit.dtb"
-dtc -I dts -O dts -o "${MY_DTS}" "${OVERLAY}" "${BASE}"
